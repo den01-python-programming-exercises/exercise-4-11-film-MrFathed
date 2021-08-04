8d1163205ae45035ad7425ba2c159c312ece5f5c
Justin Turner <jmturner638@gmail.com>
2021-08-04 07:24:59 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
+from film import Film
+
 def main():
     #write your code below this line
+    chipmunks = Film("Alvin and the Chipmunks: The Squeakquel", 0)
+    age = int(input("How old are you?"))
+
+    if (age >= chipmunks.age_rating):
+        print("You may watch the film " + chipmunks.name)
+    else:
+        print("You may not watch the film " + chipmunks.name)
+
 
 if __name__ == '__main__':
     main()
---
+++ src/film.py
@@ -0,0 +1,10 @@
+class Film:
+    def __init__(self, film_name, film_age_rating):
+        self.name = film_name
+        self.age_rating = film_age_rating
+
+    def name(self):
+        return self.name
+
+    def age_rating(self):
+        return self.age_rating
